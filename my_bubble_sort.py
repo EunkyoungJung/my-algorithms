fdb7d3cdba98223c9363ace858c11dd4362b121f
unknown <muchogusto.lol@gmail.com>
2020-06-17 11:22:03 +0900

    play with bubble sort

--- my_bubble_sort.py
+++ my_bubble_sort.py
@@ -9,10 +9,14 @@
 def bubble_sort(seq):
     length = len(seq) - 1
     for num in range(length, 0, -1):
+        swap = 0
         for i in range(num):
             if seq[i] > seq[i+1]:
                 seq[i], seq[i+1] = seq[i+1], seq[i]
-        # print(f"{num}: {seq}")
+                swap += 1
+        print(f"{num}: {seq}/swap: {swap}")
+        if swap == 0:
+            break
     return seq
 
 
