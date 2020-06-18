d0f8fc1d111568bfde55c25a6f4997a0e3c402f8
unknown <muchogusto.lol@gmail.com>
2020-06-18 10:06:36 +0900

    play with selection sort

---
+++ my_selection_sort.py
@@ -0,0 +1,29 @@
+"""
+선택정렬 (selection sort)
+- 먼저 리스트에서 가장 작거나 큰 항목을 찾아서(선택) 첫 번째 항목과 위치를 바꾼다.
+- 그러고 나서 그 다음 항목을 찾아서(선택) 두 번쩨 항목과 위치를 바꾼다
+- 이런식으로 리스트 끝에 도달할 때까지 이 과정을 반복한다.
+- 리스트가 이미 정렬되어 있어도 시간복잡도는 O(n*n) (안정적이지도 않다)
+"""
+
+
+def selection_sort(seq):
+    # 오름차순 정렬
+    length = len(seq)
+    for i in range(length-1):
+        min_j = i
+        for j in range(i+1, length):
+            if seq[min_j] > seq[j]:
+                min_j = j
+        seq[i], seq[min_j] = seq[min_j], seq[i]
+    return seq
+
+
+def test_selection_sort():
+    seq = [11, 3, 28, 43, 9, 4]
+    assert(selection_sort(seq) == sorted(seq))
+    print("테스트 통과")
+
+
+if __name__ == "__main__":
+    test_selection_sort()
