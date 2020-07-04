7195a4dd083b46415e0f22814077a70a5b7e5e02
marie <muchogusto.lol@gmail.com>
2020-07-04 16:10:02 +0900

    play with heap sort

---
+++ my_heap_sort2.py
@@ -0,0 +1,20 @@
+# from my_max_heap import Heap
+#
+# def heap_sort2(seq):
+#     s = list(seq)
+#     heap = Heap(s)
+#     res = []
+#     for _ in range(len(s)):
+#         res.insert(0, heap.extract_max())
+#     return res
+#
+#
+# def test_heap_sort2():
+#     seq = [3, 5, 2, 6, 8, 1, 0, 3, 5, 6, 2]
+#     print(f"heap_sort2(seq): {heap_sort2(seq)}")
+#     assert(heap_sort2(seq) == sorted(seq))
+#     print("테스트 통과!")
+#
+#
+# if __name__ == "__main__":
+#     test_heap_sort2()
