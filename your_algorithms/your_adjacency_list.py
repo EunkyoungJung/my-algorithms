b682b677c57919b64b3e2eb5007b5aa9bbc4e153
marie <muchogusto.lol@gmail.com>
2020-08-01 17:23:14 +0900

    play with adjacency list

--- your_algorithms/your_adjacency_list.py
+++ your_algorithms/your_adjacency_list.py
@@ -33,5 +33,22 @@ print(b in N[b]) # False
 print(len(N[f])) # 4
 
 
+# Dictionary
+"""
+<Dictionary를 이용한 인접 리스트>
+- 노드가 키가 되고, 각 노드를 간선 가중치 등의 값으로 연결할 수 있다.
+- 딕셔너리의 기본 구조를 활용하면 조금 더 유연하게 인접 리스트를 만들 수 있다.
+"""
+a, b, c, d, e, f = range(6) # 6개 노드
+N = [
+    {b: 2, c: 1, d: 4, f: 1}, {a: 4, d: 1, f: 4}, {a: 1, b: 1, d: 2, e: 4},
+    {a: 3, e: 2}, {a: 3, b: 4, c: 1}, {b: 1, c: 2, d: 4, d: 3}
+    ]
+print(b in N[a]) # True
+print(len(N[f])) # 3
+print(N[a][b]) # 2
 
-
+a, b, c, d, e, f = range(6) # 6개 노드
+N = {'a': set('bcdf'), 'b': set('adf'), 'c': set('abde'), 'd': set('ae'), 'e': set('abc'), 'f': set('bcde')}
+print('b' in N['a']) # True
+print(len(N['f'])) # 4
