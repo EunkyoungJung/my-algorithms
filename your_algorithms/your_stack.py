53feafb4fe3232b7b087753b96a3c00799df06b2
marie <muchogusto.lol@gmail.com>
2020-08-09 18:09:24 +0900

    play with stack

---
+++ your_algorithms/your_stack.py
@@ -0,0 +1,42 @@
+"""
+stack
+"""
+
+
+class Stack(object):
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self) -> bool:
+        return False if self.items else True
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop() if self.items else None
+
+    def size(self) -> int:
+        return len(self.items)
+
+    def peek(self):
+        return self.items[-1] if self.items else None
+
+    def __repr__(self):
+        return repr(self.items)
+
+
+def test_stack():
+    s = Stack()
+    assert(s.isEmpty() is True)
+    for i in range(10):
+        s.push(i)
+    assert(s.size() == 10)
+    assert(s.peek() == 9)
+    assert(s.pop() == 9)
+    assert(s.peek() == 8)
+    assert(s.isEmpty() is False)
+
+
+if __name__ == "__main__":
+    test_stack()
