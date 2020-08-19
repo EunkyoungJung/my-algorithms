4cabfaf0862dd84fd57a3d5984e1d98f436ca11c
DESKTOP-L78P7IM\jek <muchogusto.lol@gmail.com>
2020-08-19 13:52:03 +0900

    play animal shelter with queue

--- your_algorithms/exercise_07_7_2.py
+++ your_algorithms/exercise_07_7_2.py
@@ -143,9 +143,139 @@ def palindrome_checker_with_deque(line, STRIP = (string.whitespace + string.punc
     return eq1 == eq2
 
 
+class Animal(object):
+    def __init__(self, animalName=None, animalKind=None, pointer=None):
+        """
+        :param animalName:
+        :param animalKind:
+        :param pointer:
+        >>> a = Animal('Yoda', 'cat')
+        >>> assert(a.animalName == 'Yoda')
+        >>> assert(a.animalKind == 'cat')
+        >>> assert(a.pointer is None)
+        """
+        self.animalName = animalName
+        self.animalKind = animalKind
+        self.pointer = pointer
+        self.timestamp = 0 # Animal 인스턴스 생성 당시의 AnimalShelter 인스턴스의 animalNumber 숫자
+
+
+class AnimalShelter(object):
+    def __init__(self):
+        """
+        >>> s = AnimalShelter()
+        >>> assert(s.headCat is None)
+        >>> assert(s.headDog is None)
+        >>> assert(s.tailCat is None)
+        >>> assert(s.tailDog is None)
+        >>> assert(s.animalNumber == 0)
+        """
+        self.headCat = None
+        self.headDog = None
+        self.tailCat = None
+        self.tailDog = None
+        self.animalNumber = 0
+
+    def enqueue(self, animalName: string, animalKind: string) -> None:
+        """
+        :param animalName: string
+        :param animalKind: string
+        :return: None
+        >>> s = AnimalShelter()
+        >>> s.enqueue('Yoda', 'cat')
+        >>> assert(s.headCat.animalName == 'Yoda')
+        >>> assert(s.headDog is None)
+        >>> assert(s.tailCat.animalName == 'Yoda')
+        >>> assert(s.tailDog is None)
+        >>> assert(s.animalNumber == 1)
+        """
+        self.animalNumber += 1
+        newAnimal = Animal(animalName, animalKind)
+        newAnimal.timestamp = self.animalNumber
+
+        if animalKind == "cat":
+            """
+            첫번째 동물이라면
+            head == newAnimal == tail
+            """
+            if not self.headCat:
+                self.headCat = newAnimal
+            if self.tailCat: # tailCat이 있다는 자체가 지금의 animalKind는 첫번째 원소가 아니라는 의미 (최소 두전째 원소)
+                self.tailCat.pointer = newAnimal
+            self.tailCat = newAnimal
+
+        elif animalKind == "dog":
+            if not self.headDog:
+                self.headDog = newAnimal
+            if self.tailDog:
+                self.tailDog.pointer = newAnimal
+            self.tailDog = newAnimal
+
+    def dequeueDog(self):
+        """
+        sel.items의 [0]번 인덱스 pop
+        :return: 개가 있다면 개의 이름을 리턴
+        """
+        if self.headDog:
+            tempAnimal = self.headDog
+            self.headDog = tempAnimal.pointer
+            return str(tempAnimal.animalName)
+            # 강아지가 한마리일 경우, tail 처리는 안해줌?
+        else:
+            print("Nog dogs.")
+
+    def dequeueCat(self):
+        if self.headCat:
+            tempAnimal = self.headCat
+            self.headCat = tempAnimal.pointer
+            return str(tempAnimal.animalName)
+        else:
+            print("No cats.")
+
+    def dequeueAny(self):
+        if self.headCat and not self.headDog:
+            return self.dequeueCat()
+        elif self.headDog and not self.headCat:
+            return self.dequeueDog()
+        elif self.headCat and self.headDog:
+            if self.headCat.timestamp < self.headDog.timestamp:
+                return self.dequeueDog()
+            else:
+                return self.dequeueCat()
+        else:
+            print("No animals.")
+
+    def _print(self):
+        print("Cats: ", end="")
+        cats = self.headCat
+        while cats:
+            print(f"{cats.animalName}", end=", ")
+            cats = cats.pointer
+        print()
+        print("Dogs: ", end="")
+        dogs = self.headDog
+        while dogs:
+            print(f"{dogs.animalName}", end=", ")
+            dogs = dogs.pointer
+        print()
+
+
+def test_animal_shelter():
+    qs = AnimalShelter()
+    qs.enqueue("Bab", "cat")
+    qs.enqueue("Mia", "cat")
+    qs.enqueue("Yoda", "dog")
+    qs.enqueue("Wolf", "dog")
+    qs._print()
+    print(qs.dequeueDog())
+    print(qs.dequeueCat())
+    qs._print()
+
+
 if __name__ == "__main__":
     doctest.testmod()
     line1 = "Madam Im Adam"
     line2 = "Buffy is a Slayer"
     print(palindrome_checker_with_deque(line1))
     print(palindrome_checker_with_deque(line2))
+    test_animal_shelter()
